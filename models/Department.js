912eb3142c3cea25af660f453be667f87f625be2
NikoMayz <mikeayoungjr@gmail.com>
2024-06-02 11:43:09 -0400

    added seeds folder structure, completed models files

---
+++ models/Department.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Department extends Model {}
+
+Department.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING(30),
+            unique: true,
+            allowNull: false
+          }
+        },
+        {
+            sequelize,
+            timestamps: false,
+            // Prevent sequelize from renaming the table
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'department'
+          }
+        );
+
+        module.exports = Department;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+)
